77b43fc91376dca5f54fa763fb633e313d13b7b6
liuxin <13892259703@163.com>
2020-09-20 11:34:42 +0800

    register

--- src/app/common/login/login.component.ts
+++ src/app/common/login/login.component.ts
@@ -9,7 +9,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class LoginComponent implements OnInit {
 
-  baseUrl="http://218.29.137.134:22742/api/TokenAuth/Authenticate";
+  baseUrl = 'http://218.29.137.134:22742/api/TokenAuth/Authenticate';
   public userName: string;
   public pwd: string;
   public requestBody: object;
@@ -19,34 +19,31 @@ export class LoginComponent implements OnInit {
     })
   };
   public accessToken: string;
-  public time: number = 2*60*60*1000;// cookie过期时间两个小时 2*60*60*1000
-  
+  public time: number = 2 * 60 * 60 * 1000; // cookie过期时间两个小时 2*60*60*1000
   constructor(
     private http: HttpClient,
     private cookies: CookieService,
-    ) {  
-  }
+    ) {}
 
   ngOnInit(): void {
   }
-
-  login(){
+  login() {
     this.requestBody = {
-      "userNameOrEmailAddress": this.userName,
-      "password": this.pwd,
-      "twoFactorVerificationCode": "string",
-      "rememberClient": true,
-      "twoFactorRememberClientToken": "string",
-      "singleSignIn": true,
-      "returnUrl": "string",
-      "captchaResponse": "string"
-    }
-    this.http.post(this.baseUrl,this.requestBody,this.httpOptions).subscribe((res: Response)=>{
-      this.accessToken = res['result'].accessToken;
-      this.cookies.set("accessToken",this.accessToken,new Date(new Date().getTime() + this.time))
+      'userNameOrEmailAddress': this.userName,
+      'password': this.pwd,
+      'twoFactorVerificationCode': 'string',
+      'rememberClient': true,
+      'twoFactorRememberClientToken': 'string',
+      'singleSignIn': true,
+      'returnUrl': 'string',
+      'captchaResponse': 'string'
+    },
+    this.http.post(this.baseUrl, this.requestBody, this.httpOptions).subscribe((res: Response) => {
+      this.accessToken = res[ 'result' ].accessToken;
+      this.cookies.set('accessToken', this.accessToken, new Date(new Date().getTime() + this.time))
 
-      console.log(res)
-    })    
+      console.log(res);
+    });
   }
 
 }
--- src/app/common/regist/regist.component.ts
+++ src/app/common/regist/regist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-regist',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./regist.component.css']
 })
 export class RegistComponent implements OnInit {
-
-  constructor() { }
+  baseUrl = 'http://218.29.137.134:22742/api/services/app/User/CreateOrUpdateUser';
+  public requestBody: object;
+  public httpOptions = {
+  headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
+  public email: string;
+  public name: string;
+  public userName: string;
+  public psw: string;
+  public pswAgain: string;
+  public cardId: string;
+  public area: string;
+  public phoneNum: string;
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
+  register() {
+    this.requestBody = {
+      "user": {
+        "id": 0,
+        "name": this.name,
+        "surname": this.cardId,
+        "userName": this.userName,
+        "emailAddress": this.email,
+        "phoneNumber": this.phoneNum,
+        "password": this.psw,
+        "isActive": true,
+        "shouldChangePasswordOnNextLogin": false,
+        "isTwoFactorEnabled": false,
+        "isLockoutEnabled": true
+      },
+      "assignedRoleNames": [
+        "User"
+      ],
+      "sendActivationEmail": false,
+      "setRandomPassword": false,
+      "organizationUnits": [
+        0
+      ]
+    };
+    this.http.post(this.baseUrl, this.requestBody, this.httpOptions).subscribe((res) => {
+      if (res[ 'success']) {
+        alert("注册成功，请登录");
+        this.router.navigate(['/login']);
+      } else {
+        alert("出现未知错误，请重试");
+      }
 
+    })
+  }
 }
